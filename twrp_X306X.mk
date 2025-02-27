4d88cfee3177f350d51ab4d7fe7d6041b27751fd
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-02-27 20:10:14 +0100

    Update twrp_X306X.mk

--- twrp_X306X.mk
+++ twrp_X306X.mk
@@ -17,7 +17,6 @@ $(call inherit-product, device/lenovo/X306X/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X306X
