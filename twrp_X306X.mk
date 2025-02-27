7ae277196d8f925c5a11d4343a8a2b0445e31866
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-02-27 09:32:28 +0100

    Update and rename omni_X306X.mk to twrp_X306X.mk

--- twrp_X306X.mk
+++ twrp_X306X.mk
@@ -6,20 +6,22 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from X306X device
 $(call inherit-product, device/lenovo/X306X/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X306X
-PRODUCT_NAME := omni_X306X
+PRODUCT_NAME := twrp_X306X
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo TB-X306X
 PRODUCT_MANUFACTURER := lenovo
